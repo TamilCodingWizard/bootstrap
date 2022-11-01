205dc1b55db7e5a4836e4dc402acf3ccb773b9d9
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-11-01 19:17:30 +0000

    video5

--- src/App.js
+++ src/App.js
@@ -5,40 +5,15 @@ import { Container } from 'react-bootstrap';
 
 function App() {
   return (
+    <>
     <Container>
-       {/* display */}
-       {/* hide and show */}
-       <p className="m-3 p-3 text-bg-primary d-block">Display</p>
-       {/* flex */}
-       <div className="d-flex flex-column align-items-end">
-          <p className="m-2 p-2 text-bg-primary w-50 align-self-start">Flex</p>
-          <p className="m-2 p-2 text-bg-primary w-50">Flex</p>
-          <p className="m-2 p-2 text-bg-primary w-50">Flex</p>
-       </div>
-
-       <div className="d-flex justify-content-center">
-          <p className="m-2 p-2 text-bg-primary w-25 ">Flex</p>
-          <p className="m-2 p-2 text-bg-primary w-25">Flex</p>
-          <p className="m-2 p-2 text-bg-primary w-25">Flex</p>
-       </div>
-      
-      {/* Stack */}
-      <div className="vstack gap-1 align-items-center">
-        <p className="m-3 p-3 text-bg-secondary w-50">Vstack</p>
-        <p className="m-3 p-3 text-bg-secondary w-50">Vstack</p>
-        <p className="m-3 p-3 text-bg-secondary w-50">Vstack</p>
-      </div>
-
-      <div className="hstack justify-content-center gap-1">
-          <p className="m-2 p-2 text-bg-secondary w-25 ">Hstack</p>
-          <p className="m-2 p-2 text-bg-secondary w-25">Hstack</p>
-          <p className="m-2 p-2 text-bg-secondary w-25">Hstack</p>
-       </div>
-     
-      {/* shadow */}
-      <div className="shadow-none bg-light m-3 p-3">Shadow</div>
-      
+      <div className="text-bg-primary">Hello</div>
     </Container>
+
+    <Container fluid ='lg'>
+      <div className="text-bg-secondary mt-3">Fluid</div>
+    </Container>
+    </>
   );
 }
 
