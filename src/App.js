f65330d20191947848eb2ce919c80b0bceecc1b5
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-11-04 21:34:07 +0000

    project 01

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "./css/main.min.css";
-import Button from "react-bootstrap/Button";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Popover from "react-bootstrap/Popover";
-import { Container, Row, Col } from "react-bootstrap";
+import AppBar from "./components/AppBar";
+
 
 function App() {
-  const popover = (
-    <Popover id="popover-basic">
-      <Popover.Header as="h3">Popover right</Popover.Header>
-      <Popover.Body>
-        And here's some <strong>amazing</strong> content. It's very engaging.
-        right?
-      </Popover.Body>
-    </Popover>
-  );
+  
   return (
-    <Container>
-      <p className="text-bg-primary">Primary color</p>
-
-      <Button variant="primary">Primary</Button>
-
-      <OverlayTrigger trigger="click" placement="right" overlay={popover}>
-        <Button variant="success">Click me to see</Button>
-      </OverlayTrigger>
-    </Container>
+    <AppBar/>
   );
 }
 
