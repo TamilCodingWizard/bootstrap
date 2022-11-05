e61fe33b0f504f0ae6ca6d36fbf0c9a881bef3d0
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-11-05 21:19:21 +0000

    project02

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import AppBar from "./components/AppBar";
-
+import Banner from "./components/Banner";
 
 function App() {
-  
   return (
-    <AppBar/>
+    <>
+      <AppBar />
+      <Banner />
+    </>
   );
 }
 
