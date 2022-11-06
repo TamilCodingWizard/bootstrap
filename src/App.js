3a91c80f70ee23a0d0d997b486c35cbdbe8abbec
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-11-06 18:04:28 +0000

    card

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import AppBar from "./components/AppBar";
 import Banner from "./components/Banner";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <>
       <AppBar />
       <Banner />
+      <Menu/>
     </>
   );
 }
