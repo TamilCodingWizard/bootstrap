2c66a4e3ac8e1b7ac6b2b56de045f43adf0dc32e
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-11-07 20:54:13 +0000

    project04

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import AppBar from "./components/AppBar";
 import Banner from "./components/Banner";
 import Menu from "./components/Menu";
+import PickoftheWeek from "./components/PickoftheWeek";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <AppBar />
       <Banner />
       <Menu/>
+      <PickoftheWeek/>
     </>
   );
 }
